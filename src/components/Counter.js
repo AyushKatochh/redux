6057e06818c9426e2bba7c822c760c95c26414e2
Ayush Katoch <katochayushhp@gmail.com>
2022-02-21 13:44:39 +0530

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,35 +1,30 @@
-import {useSelector, useDispatch} from "react-redux";
-
-import { counterActions } from "../store/index";
-
-
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
+import { counterActions } from '../store/index';
 
 const Counter = () => {
-  const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter.counter);
-  const show = useSelector(state => state.counter.showCounter);
-  
-  const incrementHandler = () => {
-    dispatch(counterActions.increment());
-  }
+ const dispatch = useDispatch();
+ //state => state.counter.counter 1st counter defines configure store slice
+ const counter =  useSelector(state => state.counter.counter);
+ const show = useSelector(state => state.counter.showCounter);
 
-  const increaseHandler = () => {
-    // Setting payload(data) to actions
-    // increase(10)
-    dispatch(counterActions.increase(10)); // {type: SOME_UNIQUE_IDENTIFIER, payload: 10}
-  }
-  
+ const incrementHandler = () => {
+     dispatch(counterActions.increment());
+ };
 
+ const increaseHandler = () => {
+   //{type: SOME_UNIQUE_IDENTIFIER, payload: any}
+   dispatch(counterActions.increase(5));
+ }
 
-  const decrementHandler = () => {
-    dispatch(counterActions.decrement());
-  }
-  
+ const decrementHandler = () => {
+     dispatch(counterActions.decrement());
+ }
+
   const toggleCounterHandler = () => {
-    dispatch(counterActions.toggleCounter());
+    dispatch(counterActions.toggleCounter())
   };
 
   return (
@@ -37,10 +32,9 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       {show && <div className={classes.value}>{counter}</div>}
       <div>
-        <button onClick={incrementHandler}>Increment</button>  
-        <button onClick={increaseHandler}>Increase by 10</button> 
+        <button onClick={incrementHandler}>Increment</button>
+         <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
-
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
@@ -48,4 +42,3 @@ const Counter = () => {
 };
 
 export default Counter;
- 
